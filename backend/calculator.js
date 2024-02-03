d874bd8fbaa46decd6845f292358240b2381f570
Michael Mgebrishvili <mmd920802@gmail.com>
2024-02-03 23:21:38 +0300

    back + front

---
+++ backend/calculator.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+
+function calculator(req, res) {
+    const numbers = req.body.value;
+
+    const dataCalc = fs.readFileSync(path.join(__dirname, 'data-calc.json'), 'utf-8');
+    const valueCalcObject = JSON.parse(dataCalc);
+
+    if (req.url === '/value') {
+        valueCalcObject.value = 0;
+    }
+
+    if (req.url === '/plus') {
+        valueCalcObject.value = Number(valueCalcObject.value) + Number(numbers);
+    }
+
+    if (req.url === '/minus') {
+        valueCalcObject.value = Number(valueCalcObject.value) - Number(numbers);
+    }
+
+    if (req.url === '/multiply') {
+        valueCalcObject.value = Number(valueCalcObject.value) * Number(numbers);
+    }
+
+    if (req.url === '/divide') {
+        valueCalcObject.value = Number(valueCalcObject.value) / Number(numbers);
+    }
+
+
+    fs.writeFileSync(path.join(__dirname, 'data-calc.json'), JSON.stringify(valueCalcObject), 'utf-8');
+    const dataCalcNew = fs.readFileSync(path.join(__dirname, 'data-calc.json'), 'utf-8');
+    res.json(JSON.parse(dataCalcNew));
+}
+
+module.exports = { calculator };
--- backend/index.js
+++ backend/index.js
@@ -5,58 +5,33 @@ const port = 3000;
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
+const { calculator } = require('./calculator');
 
 app.use(cors());
 app.use(bodyParser.json());
-
 
 app.get('/', (req, res) => {
     res.send('Это Express, а я - разработчик');
 });
 
 app.get('/value', (req, res) => {
-    const dataCalc = fs.readFileSync(path.join(__dirname, 'data-start.json'), 'utf-8');
-    const valueCalcStart = JSON.parse(dataCalc);
-    res.json(valueCalcStart);
+    calculator(req, res)
 });
 
 app.post('/plus', (req, res) => {
-    /////РАЗОБРАТЬСЯ
-    const numbers = req.body.value;
-    console.log(numbers);
-
-    const dataCalc = fs.readFileSync(path.join(__dirname, 'data-calc.json'), 'utf-8');
-    const valueCalcPlus = JSON.parse(dataCalc);
-
-    valueCalcPlus.value = Number(valueCalcPlus.value) + Number(numbers);
-
-    const dataCalcNew = fs.writeFileSync(path.join(__dirname, 'data-calc.json'), JSON.stringify(valueCalcPlus), 'utf-8');
-
-    res.json(dataCalcNew);
+    calculator(req, res)
 });
 
 app.post('/minus', (req, res) => {
-    const numbers = req.body;
-
-    const dataCalc = fs.readFileSync(path.join(__dirname, 'data-calc.json'), 'utf-8');
-    const valueCalcMinus = JSON.parse(dataCalc);
-    res.json(valueCalcMinus);
+    calculator(req, res)
 });
 
 app.post('/multiply', (req, res) => {
-    const numbers = req.body;
-
-    const dataCalc = fs.readFileSync(path.join(__dirname, 'data-calc.json'), 'utf-8');
-    const valueCalcMultiply = JSON.parse(dataCalc);
-    res.json(valueCalcMultiply);
+    calculator(req, res)
 });
 
 app.post('/divide', (req, res) => {
-    const numbers = req.body;
-
-    const dataCalc = fs.readFileSync(path.join(__dirname, 'data-calc.json'), 'utf-8');
-    const valueCalcDivide = JSON.parse(dataCalc);
-    res.json(valueCalcDivide);
+    calculator(req, res)
 });
 
 
